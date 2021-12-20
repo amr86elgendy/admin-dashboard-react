17ac2ef704ebc944c22c6785499c324c14f37d07
amr elgendy <amr86elgendy@gmail.com>
2021-12-20 02:46:03 +0200

    fix

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -3,7 +3,11 @@ import { useMutation } from 'react-query';
 const login = (user) =>
   fetch(`${process.env.REACT_APP_SERVER_URL}/api/auth/login`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'same-origin',
     body: JSON.stringify(user),
   });
 

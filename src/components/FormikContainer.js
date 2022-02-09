d513063bfb3f2b47de32a0547fad5231d0bfa4b6
amr elgendy <amr86elgendy@gmail.com>
2022-02-09 14:02:03 +0200

    fixxx

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -7,25 +7,21 @@ const FormikContainer = ({
   title,
   children,
 }) => {
-  
   return (
     <Formik
-      isInitialValid
       initialValues={initialValues}
       validate={validate}
       onSubmit={onSubmit}
       enableReinitialize
     >
-      {(props) => {
-        console.log(onSubmit);
-        return (
+      {(props) => (
         <Form>
           <h2 className='mb-8 tracking-widest uppercase text-[#8094ae]'>
             {title}
           </h2>
           <div className='grid grid-cols-6 gap-6 mb-8'>{children}</div>
         </Form>
-      )}}
+      )}
     </Formik>
   );
 };
